fa87c24f58b3fcf582c7b92f986d61e57d8da762
doloresespanol <popokaja9@gmail.com>
2022-11-10 11:15:59 +0100

    polska

--- app/src/main/java/com/example/sedzia/MainActivity.java
+++ app/src/main/java/com/example/sedzia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sedzia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -21,14 +22,21 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         punktyViewModel=new ViewModelProvider(this).get(PunktyViewModel.class);
-        binding.textView1.setText(String.valueOf(punktyViewModel.getPunkty()));
+        getSupportActionBar().setTitle("Aplikacja sędziowska");
+        punktyViewModel.getPunkty().observe(this,
+                new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer integer) {
+                    binding.textView1.setText(String.valueOf(integer));
+                    }
+                });
+
         //dzieki bindowaniu widokow nie musimy robic findviewbyid
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //punkty++;
                 punktyViewModel.dodajPunkty(1);
-                binding.textView1.setText(String.valueOf(punktyViewModel.getPunkty()));
             }
         });
         binding.button2.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //punkty++;
                 punktyViewModel.dodajPunkty(2);
-                binding.textView1.setText(String.valueOf(punktyViewModel.getPunkty()));
             }
         });
         binding.button3.setOnClickListener(new View.OnClickListener() {
@@ -44,7 +51,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //punkty++;
                 punktyViewModel.dodajPunkty(3);
-                binding.textView1.setText(String.valueOf(punktyViewModel.getPunkty()));
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //punkty++;
+                punktyViewModel.setPunkty(0);
             }
         });
     }
